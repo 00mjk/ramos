75cee238207caf06f25656d1f88c3eeeeaef5256
hanatic <71867548+hanatic@users.noreply.github.com>
2021-07-27 15:30:14 +0100

    Create Client.ts

---
+++ src/utils/Client.ts
@@ -0,0 +1,26 @@
+import { Client } from "discord.js";
+import { Package } from "./Packages";
+
+export type ClientMode = "stable" | "canary" | "dev";
+
+export default class Hyview extends Client {
+
+    public prefix: string;
+    public mode: ClientMode;
+    public packages: Array<Package> = Array<Package>();
+    public doDummyOrders: boolean;
+
+    constructor(options: ClientOptions) {
+        super();
+        this.prefix = options.prefix;
+        this.mode = options.mode;
+        this.doDummyOrders = options.doDummyOrders;
+    }
+
+}
+
+export interface ClientOptions {
+    prefix: string,
+    mode: ClientMode,
+    doDummyOrders: boolean
+}
