78111cdde40e33ddd34c1fffb6de96bcf4b6f605
hanatic <71867548+hanatic@users.noreply.github.com>
2021-07-27 15:34:38 +0100

    Create Constants.ts

---
+++ src/utils/Constants.ts
@@ -0,0 +1,12 @@
+const Constants = {
+    ColorCodes: {
+      Neutral: "D3D3D3",
+      Info: "00A6FF",
+      Success: "11D200",
+      Warning: "FFAA00",
+      Danger: "D90000",
+      Error: "7C0000"
+    }
+}
+
+export default Constants;
